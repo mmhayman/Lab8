c69639b3c51c99b9f9e52304839829e042623e01
mmhayman <mmhayman@svsu.edu>
2017-07-07 15:54:48 -0400

    finished project

--- src/Lab8.java
+++ src/Lab8.java
@@ -1,3 +1,4 @@
+
 import java.util.Scanner;
 
 
@@ -6,62 +7,89 @@ import java.util.Scanner;
  */
 public class Lab8 {
 
+    public static int numAtBats;
+
     public static void main(String[] args) {
 
         Scanner scan = new Scanner(System.in);
 
         String choice = "y";
 
-
         while (choice.equalsIgnoreCase("y")) {
 
             System.out.println("Welcome to Batting Average Calendar \n");
 
-            System.out.print("Enter number of times at bat: ");
-
+            System.out.print("Enter number of times at bat: "); // enter number of times up to bat
             int numAtBats = scan.nextInt();
 
-            while (numAtBats < 0) {
+            while (numAtBats <0) { // validating user input
                 System.out.print("Invalid Input. Please try again: ");
                 numAtBats = scan.nextInt();
             }
-            int[] arr = new int[numAtBats];
-            getResults(scan, arr);
-            getAtBatResults(scan, arr);
 
+
+            System.out.println("0 = out, 1 = single, 2 = double, 3 = triple , 4 = home run");
             System.out.println();
 
-            System.out.println("Another batter? (y/n) ");
-            choice = scan.nextLine();
-        }
-    }
+            int[] arr = new int[numAtBats]; // array
 
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print("Result for at- bat " + (i + 1) + ": "); // result up to bat
+                int numBasesEarned = scan.nextInt();
 
-    public static void getResults(Scanner scan, int[] arr) {
+                if (numBasesEarned < 0 ) { // validating user input
+                    System.out.println("You entered an invalid number. Please try again: 0 - 4 ");
+                    numBasesEarned = scan.nextInt();
+                    if (numBasesEarned > 5) {
+                        System.out.println("You entered an invalid number Pleaes try again: 0 - 4");
+                    }
 
-        double totalBases = 0;
-        for (int i = 0; i < arr.length; i++) {
-            totalBases = totalBases + arr[i];
-
-        }
-
-
-    }
-
-    public static void getAtBatResults(Scanner scan, int[] arr) {
-
-        int numBasesEarned = 0;
-        System.out.println("0 = out, 1 = single, 2 = double, 3 = triple , 4 = home run");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print("Result for at- bat " + i + ": ");
-            numBasesEarned = scan.nextInt();
-
-            while (numBasesEarned > 0 && numBasesEarned > 5) {
-                System.out.println("You entered an invalid number. Please try again: ");
-                numBasesEarned = scan.nextInt();
+                }
+                arr[i] = numBasesEarned;
             }
 
+
+            double atBatTotal = 0;
+            double totalSumAtBat = 0;
+            double sluggingPercentage;
+            double battingAverage;
+
+            for (int i = 0; i < numAtBats; i++) { // finding batting average
+
+                if (arr[i] > 0) {
+                    atBatTotal += 1;
+
+                }
+
+            }
+            battingAverage = (atBatTotal / numAtBats);
+
+            System.out.printf("Batting Average: " + "%.3f", +battingAverage, "%n");// printing B.A
+            System.out.println();
+
+            for (int i : arr) // finding slugging percentage
+
+            {
+                if (i > 0) {
+                    totalSumAtBat+= i;
+
+                }
+
+            }
+
+            sluggingPercentage = (totalSumAtBat / numAtBats);
+            System.out.printf("Slugging Percentage: " + "%.3f", +sluggingPercentage, "%n");// printing S.P
+
+            System.out.println();
+            System.out.println("Another batter? (y/n) "); // asking user to continue
+            choice = scan.next();
+            choice.toLowerCase();
         }
+        System.out.println("goodbye");
 
     }
 }
+
+
+
+
